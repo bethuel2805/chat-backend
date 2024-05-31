444a27efb0170a1ed96815e7a246c5a9c709ee60
bethuel <bethuel2122@gmail.com>
2024-05-31 15:55:39 +0200

    adding socket

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = 5000
 
 const io = require("socket.io")(server)
 
+io.on("connect",(socket)=>{
+    console.log("Socket connected : "+socket.id);
+})
+
 app.listen(PORT,()=>{
     console.log("Server Start...");
 })
